1e07e8b2a7dd5f9f88076a1f60a96b1c91fe2adb
Yitong <charles.yitong@gmail.com>
2024-01-15 22:56:39 +1100

    create function to import fake data

--- basketball_league/create_fake_data.py
+++ basketball_league/create_fake_data.py
@@ -1,5 +1,11 @@
-def main():
-    fake: Faker = Faker()
+def generate_match(teams):
+    matches = []
+    random.shuffle(teams)
+    for i in range(0, len(teams), 2):
+        matches.append([teams[i], teams[i+1]])
+    return matches
+
+def create_fake_team_users(fake):
     player_role = Role.objects.get(name='Player')
     coach_role = Role.objects.get(name='Coach')
     league_admin_role = Role.objects.get(name='League Admin')
@@ -43,6 +49,63 @@ def main():
             player.set_password('12345')
             player.save()
 
+def create_tournament(fake):
+    Tournament.objects.all().delete()
+    tournament = Tournament.objects.create(
+        name=fake.sentence(nb_words=2) + ' touranment'
+    )
+    teams_left = [team for team in Team.objects.prefetch_related('members').all()]
+    current_round = 1
+    while len(teams_left) > 1:
+
+        matches = generate_match(teams_left)
+        teams_left = []
+        round_obj = Round.objects.create(
+            name="round-" + str(current_round),
+            tournament=tournament
+        )
+        current_round += 1
+        for team_1, team_2 in matches:
+            game = Game.objects.create(
+                name=team_1.name + ' vs ' + team_2.name,
+                round=round_obj,
+                location=fake.sentence(nb_words=1) + ' location'
+            )
+            team_1_player = [(member, random.randint(0, 2)) for member in team_1.members.all() if member.role.name == Role.player]
+            team_2_player = [(member, random.randint(0, 2)) for member in team_2.members.all() if member.role.name == Role.player]
+            score_team1 = 0
+            score_team2 = 0
+            for player, score in team_1_player:
+                GamePlayerAssociation.objects.create(
+                    game=game,
+                    player=player,
+                    score=score
+                )
+                score_team1 += score
+            for player, score in team_2_player:
+                GamePlayerAssociation.objects.create(
+                    game=game,
+                    player=player,
+                    score=score
+                )
+                score_team2 += score
+            GameTeamAssociation.objects.create(
+                game=game,
+                team=team_1,
+                score=score_team1
+            )
+            GameTeamAssociation.objects.create(
+                game=game,
+                team=team_2,
+                score=score_team2
+            )
+            winner = team_1 if score_team1 > score_team2 else team_2
+            teams_left.append(winner)
+
+def main():
+    fake: Faker = Faker()
+    create_fake_team_users(fake)
+    create_tournament(fake)
  
 if __name__ == "__main__":
     import os
@@ -52,8 +115,11 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basketball_league.settings")
     application = get_wsgi_application()
 
+    import random
+
     from account.models import Account, Role, Team
     from faker import Faker
-    from game.models import Game
+    from game.models import (Game, GamePlayerAssociation, GameTeamAssociation,
+                             Round, Tournament)
  
     main()
