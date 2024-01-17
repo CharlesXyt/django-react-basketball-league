957d80af275276942ebb4711e7ec93ff00b4015e
Yitong <charles.yitong@gmail.com>
2024-01-17 16:10:01 +1100

    update requirements file and add one more test

--- basketball_league/account/tests/test_models.py
+++ basketball_league/account/tests/test_models.py
@@ -1,7 +1,7 @@
-from account.factory.account_factory import (AccountFactory, RoleFactory,
-                                             TeamFactory)
-from account.models import Account, Role, Team
+from account.factory.account_factory import AccountFactory, RoleFactory, TeamFactory
+from account.models import Role
 from django.test import TestCase
+from game.factory.game_factory import GameFactory, GamePlayerAssociationFactory
 
 
 class TeamModelTest(TestCase):
@@ -17,3 +17,15 @@ class TeamModelTest(TestCase):
         # Test if the get_team_coach method returns the correct coach account
         coach_account = self.team.get_team_coach()
         self.assertEqual(coach_account, self.coach_user)
+
+
+class AccountModelTest(TestCase):
+    def setUp(self):
+        self.player = AccountFactory(role=RoleFactory(name=Role.player))
+        GamePlayerAssociationFactory(game=GameFactory(), player=self.player, score=2)
+        GamePlayerAssociationFactory(game=GameFactory(), player=self.player, score=10)
+
+    def test_get_player_match_info(self):
+        match_info = self.player.get_player_match_info()
+        self.assertEqual(match_info.get("average_score"), 6)
+        self.assertEqual(match_info.get("game_count"), 2)
--- basketball_league/game/tests/test_models.py
+++ basketball_league/game/tests/test_models.py
@@ -1,7 +1,12 @@
-from account.factory.account_factory import AccountFactory, TeamFactory
+from account.factory.account_factory import AccountFactory, RoleFactory, TeamFactory
+from account.models import Role
 from django.test import TestCase
-from game.factory.game_factory import (GameFactory, GameTeamAssociationFactory,
-                                       RoundFactory, TournamentFactory)
+from game.factory.game_factory import (
+    GameFactory,
+    GamePlayerAssociationFactory,
+    GameTeamAssociationFactory,
+    RoundFactory,
+)
 
 
 class RoundModelTest(TestCase):
@@ -14,26 +19,34 @@ class RoundModelTest(TestCase):
         self.round.get_game_count()
         self.assertEqual(self.round.get_game_count(), 2)
 
+
 class GameModelTest(TestCase):
     def setUp(self):
-        # Create test objects
-        self.round_instance = RoundFactory(tournament=TournamentFactory())
-
         self.team_1 = TeamFactory()
         self.team_2 = TeamFactory()
         self.game = GameFactory()
-        
+        self.team_1 = TeamFactory()
+        self.team_2 = TeamFactory()
         GameTeamAssociationFactory(team=self.team_1, game=self.game, score=5)
         GameTeamAssociationFactory(team=self.team_2, game=self.game, score=6)
-
 
     def test_get_team_scores(self):
         team_scores = self.game.get_team_scores()
         self.assertEqual(len(team_scores), 2)
         for team_score in team_scores:
             if team_score.get("team_id") == self.team_1.id:
-                self.assertEqual(team_score.get("score"), 5) 
+                self.assertEqual(team_score.get("score"), 5)
                 self.assertEqual(team_score.get("is_winner"), False)
             else:
-                self.assertEqual(team_score.get("score"), 6) 
+                self.assertEqual(team_score.get("score"), 6)
                 self.assertEqual(team_score.get("is_winner"), True)
+
+    def test_get_player_detail(self):
+        self.player_1 = AccountFactory(role=RoleFactory(name=Role.player), team=self.team_1)
+        self.player_2 = AccountFactory(role=RoleFactory(name=Role.player), team=self.team_2)
+        GamePlayerAssociationFactory(game=self.game, player=self.player_1, score=5)
+        GamePlayerAssociationFactory(game=self.game, player=self.player_2, score=6)
+        player_scores = self.game.get_player_detail(self.team_1)
+
+        self.assertEqual(len(player_scores), 1)
+        self.assertEqual(player_scores[0].get("score"), 5)
